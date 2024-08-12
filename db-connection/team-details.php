bef83a5f89f25dc29faedfb2ea084c659751b496
bipulsardar421 <bipulsardar421@gmail.com>
2024-08-12 16:45:32 +0530

    .

--- db-connection/team-details.php
+++ db-connection/team-details.php
@@ -6,14 +6,13 @@ include "../db-connection/main-connection-db-model.php";
 if ($conn->connect_error) {
     die(json_encode(["error" => "Connection failed: " . $conn->connect_error]));
 }
-
-// Query to fetch all employees
 $sql = "SELECT 
             mainEmp.user_id, mainEmp.fname, mainEmp.lname, mainEmp.email, mainEmp.phone_no, mainEmp.dept,
             details.gender, details.dob, details.maritial_status, details.physically_handicapped, details.blood_group, details.nationality,
             edu.type as eduType, edu.branch, edu.cgpa, edu.yop, edu.yoj,
             contact.personal_mobile_number, contact.personal_email, contact.residence_number,
-            addr.type as addrType, addr.address, addr.city, addr.country, addr.post_code
+            addr.type as addrType, addr.address, addr.city, addr.country, addr.post_code,
+            emp_img.url
         FROM 
             employees AS mainEmp
         LEFT JOIN 
@@ -23,7 +22,11 @@ $sql = "SELECT
         LEFT JOIN 
             employees_personal_contact_details AS contact ON mainEmp.user_id = contact.user_id
         LEFT JOIN 
-            employees_address AS addr ON mainEmp.user_id = addr.user_id";
+            employees_address AS addr ON mainEmp.user_id = addr.user_id
+        LEFT JOIN
+            emp_image AS emp_img on mainEmp.user_id = emp_img.user_id
+        WHERE status = 'active'";
+
 
 $result = $conn->query($sql);
 
--- list.php
+++ list.php
@@ -21,7 +21,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script>
         document.addEventListener("DOMContentLoaded", function () {
-            fetch('./src/team-details.php')
+            fetch('./db-connection/team-details.php')
                 .then(response => response.json())
                 .then(data => {
                     const employeeCards = document.getElementById('employee-cards');
